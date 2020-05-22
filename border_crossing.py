85c7696701190693ea82163d204e8541a13984ba
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-05-22 21:26:20 +0200

    Bokeh plot basic version working

--- border_crossing.py
+++ border_crossing.py
@@ -1,15 +1,14 @@
-import numpy as np
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
 import pandas as pd
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 8, 9, 10]
-print(x)
-print(y)
+data = pd.read_excel('data/crossings_per_year.xlsx')
+
+x = data["year"]
+y = data["people_crossing_by_year"]
 
 # prepare the output file
-output_file("line.html")
+output_file("line_from_excel.html")
 
 # create a figure object
 f = figure()
@@ -17,27 +16,7 @@ f = figure()
 # create a line plot
 f.line(x, y)
 
-print("Border Crossing")
+# write the graph in the figure object
+show(f)
 
-# Testing
-with open("./data/wonderland.txt", 'r') as crossings:
-    lines = crossings.readlines()
-for line in lines:
-    print(line, end='')
-
-# # prepare some data
-# df = pd.read_excel("/data/crossings_per_year.xlsx")
-# x = df["month"]
-# y = df["people_crossing_by_month"]
-#
-# # prepare the output file
-# output_file("line_from_excel.html")
-#
-# # create a figure object
-# f = figure()
-#
-# # create a line plot
-# f.line(x, y)
-#
-# # write the graph in the figure object
-# show(f)
+# print(data)
