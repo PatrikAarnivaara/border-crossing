c13523a90cac0d60b7bb6a006f08912132381b6f
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-06-09 15:56:53 +0200

    More...

--- bokeh_graph.py
+++ bokeh_graph.py
@@ -16,23 +16,19 @@ class Graph:
         y = self.data[y]
         y2 = self.data[y2]
 
-        # random number in between -1 and 1
-        # y3 = np.random.uniform(low=-1, high=1, size=1)
-        # y_random = y * y3
-
         # prepare the output file
         output_file("legend_labels.html")
 
         # create a figure object
         f = figure(title='Border Crossings - People and Money', y_range=(300000000, 380000000))
+        f.xaxis.axis_label = "Year"
+        f.yaxis.axis_label = "People"
         f.extra_y_ranges = {"y2": Range1d(start=20000000000, end=40000000000)}
         f.add_layout(LinearAxis(y_range_name="y2", axis_label="Money"), 'right')
 
         # create a line plot
         f.line(X, y, legend_label="people", color="red")
-        # f.line(X, y_random, legend_label="random", color="pink")
         f.line(X, y2, legend_label="money", color="green", y_range_name="y2")
-        f.xaxis.axis_label = "Year"
         f.legend.location = "bottom_right"
 
         # write the graph in the figure object
--- border_crossing.py
+++ border_crossing.py
@@ -1,5 +1,5 @@
-from graph import *
-from polynomial import *
+from bokeh_graph import *
+from matplotlib_polynomial import *
 
 # reads excel-file into a variable
 data_year = Graph('data/crossings_and_remittance_per_year.xlsx')
@@ -9,7 +9,9 @@ data_year.linear_axis("year", "people", "money")
 
 # insert dataset with x-axis as first column and y-axis as last column
 data_month = Polynomial('data/crossing_remittance_by_month.xlsx')
+data_year_reg = Polynomial('data/crossings_per_year.xlsx')
 
 # insert polynomial degree
 data_month.polynomial_axis()
+data_year_reg.polynomial_axis()
 
