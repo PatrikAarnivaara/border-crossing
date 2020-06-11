1365a5d707fa1d7c8b5d0b6e090cb16e392d4fe2
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-06-11 21:03:04 +0200

    Added random_value and constant to generate random deviation

--- border_crossing.py
+++ border_crossing.py
@@ -8,10 +8,9 @@ data_year = Graph('data/crossings_and_remittance_per_year.xlsx')
 data_year.linear_axis("year", "people", "money")
 
 # insert dataset, make sure x-axis is first column and y-axis is last column
-data_month = Polynomial('data/crossing_remittance_by_month.xlsx')
+# data_month = Polynomial('data/crossing_remittance_by_month.xlsx')
 data_year_reg = Polynomial('data/crossings_per_year.xlsx')
 
 # functions plotting data
-data_month.polynomial_axis()
+# data_month.polynomial_axis()
 data_year_reg.polynomial_axis()
-
--- matplotlib_polynomial.py
+++ matplotlib_polynomial.py
@@ -22,6 +22,8 @@ class Polynomial:
 
 
 def train_test_visualize(x, y):
+    # random numbers in between -1 and 1
+    y_random = np.random.uniform(low=-1, high=1, size=len(y))
 
     # Split dataset into Train and Test set
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=0)
@@ -39,9 +41,22 @@ def train_test_visualize(x, y):
     # Evaluate the R2-value of model
     print(r2_score(y_test, y_pred))
 
+    # Refactor, 1 betyder - 1.0, 2 betyder -0.6, 3 betyder -0.2, 4 betyder +0.2, 5 betyder +0.6, 6 betyder +1
+    # snurrar ett roulette hjul 1000 gånger
+    # för statisktik på vilka nummer ni får... blir de jämt fördelade? Om man kastar tärningen 100000 gånger?
+    # om man snurrar rouletten 1 miljon gånger
+
+    xyz = regressor.predict(poly_reg.fit_transform(x))
+    a = xyz + (y_random * 5000000)
+    standard_deviation = xyz.std()
+    print(y_random)
+    print(y)
+    print(xyz)
+    print(standard_deviation)
+
     # Visualising the Polynomial Regression results
     plt.plot(x, y, color='red')
-    plt.scatter(x, regressor.predict(poly_reg.fit_transform(x)), color='blue')
+    plt.plot(x, a, color='blue')
     plt.suptitle('R2: ' + str(r2_score(y_test, y_pred)))
     plt.title('Border Remittance Polynomial')
     plt.xlabel('Year')
