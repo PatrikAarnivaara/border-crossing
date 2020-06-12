cea8830da8d807c83c3da9da2688b2a7d0995171
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-06-12 15:03:38 +0200

    Added error bars and plot grid

--- border_crossing.py
+++ border_crossing.py
@@ -5,12 +5,12 @@ from matplotlib_polynomial import *
 data_year = Graph('data/crossings_and_remittance_per_year.xlsx')
 
 # Insert desired x-axis, y-axis and twin y-axis to plot graph.
-data_year.linear_axis("year", "people", "money")
+# data_year.linear_axis("year", "people", "money")
 
 # Insert dataset for Matplotlib plot, make sure x-axis is first column and y-axis is last column.
 data_month = Polynomial('data/crossing_remittance_by_month.xlsx')
 data_year_reg = Polynomial('data/crossings_per_year.xlsx')
 
 # Functions plotting data.
-data_month.polynomial_axis()
+# data_month.polynomial_axis()
 data_year_reg.polynomial_axis()
--- matplotlib_polynomial.py
+++ matplotlib_polynomial.py
@@ -25,6 +25,9 @@ def train_test_visualize(x, y):
     # Random numbers in between -1 and 1.
     random_number = np.random.uniform(low=-1, high=1, size=len(y))
 
+    # Constant
+    const_number = 10000000
+
     # Split dataset into train and test set.
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=0)
 
@@ -44,24 +47,56 @@ def train_test_visualize(x, y):
     # Extra random generator based on a six-sided dice.
     # Refactor, 1 betyder - 1.0, 2 betyder -0.6, 3 betyder -0.2, 4 betyder +0.2, 5 betyder +0.6, 6 betyder +1
     # Implement roulette wheel one million times statistic on spread of numbers, how?
+    # Standard_deviation = y_reg.std()
 
     # Creates a new array with random numbers.
     y_reg = regressor.predict(poly_reg.fit_transform(x))
-    y_deviation = y_reg + (random_number * 5000000)
-    standard_deviation = y_reg.std()
+    y_error = random_number * const_number
+    y_deviation = y_reg + random_number * const_number
 
     # Print arrays and random number.
     print(y)
     print(y_reg)
     print(random_number)
-    print(standard_deviation)
+    # plt.suptitle('R2: ' + str(r2_score(y_test, y_pred))) 
 
-    # Visualizing results of the polynomial regression.
+    # Visualizing results of the polynomial regression with subplots.
+    plt.suptitle('Border Crossings - People - Polynomial')
+
+    # Dataset
+    plt.subplot(231)
     plt.plot(x, y, color='red')
-    plt.plot(x, y_deviation, color='blue')
-    plt.plot(x, y_reg, color='green')
-    plt.suptitle('R2: ' + str(r2_score(y_test, y_pred)))
-    plt.title('Border Remittance Polynomial')
     plt.xlabel('Year')
-    plt.ylabel('Money Remittance')
+
+    # Trained dataset regression
+    plt.subplot(232)
+    plt.plot(x, y, color='red')
+    plt.plot(x, y_reg, color='green')
+    plt.xlabel('Year')
+
+    # Dataset with random deviation (0 - 10000000)
+    plt.subplot(233)
+    plt.plot(x, y, color='red')
+    plt.plot(x, y_deviation, color='purple')
+    plt.xlabel('Year')
+
+    # Data set with errorbars random deviation (0 - 10000000)
+    plt.subplot(234)
+    plt.plot(x, y, color='red')
+    plt.errorbar(x, y, yerr=y_error, fmt='o', ecolor="black", capsize=3, alpha=0.5)
+    plt.xlabel('Year')
+
+    # Data set with random deviation by dice
+    plt.subplot(235)
+    plt.plot(x, y, color='red')
+    plt.errorbar(x, y, yerr=y_error, fmt='o', ecolor="black", capsize=3, alpha=0.5)
+    plt.xlabel('Year')
+
+    # Data set with
+    plt.subplot(236)
+    plt.plot(x, y, color='red')
+    plt.errorbar(x, y, yerr=y_error, fmt='o', ecolor="black", capsize=3, alpha=0.5)
+    plt.xlabel('Year')
+
+    # plt.ylabel('Money Remittance')
     plt.show()
