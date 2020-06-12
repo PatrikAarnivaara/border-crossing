ef1e7c66cca76858db417daab980b51e37349ed1
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-06-12 08:54:47 +0200

    Refactored and added comments.

--- border_crossing.py
+++ border_crossing.py
@@ -1,16 +1,16 @@
 from bokeh_graph import *
 from matplotlib_polynomial import *
 
-# reads excel-file into a variable
+# Reads excel-file into a variable for Bokeh plot.
 data_year = Graph('data/crossings_and_remittance_per_year.xlsx')
 
-# insert desired x-axis, y-axis and twin y-axis to plot graph
+# Insert desired x-axis, y-axis and twin y-axis to plot graph.
 data_year.linear_axis("year", "people", "money")
 
-# insert dataset, make sure x-axis is first column and y-axis is last column
-# data_month = Polynomial('data/crossing_remittance_by_month.xlsx')
+# Insert dataset for Matplotlib plot, make sure x-axis is first column and y-axis is last column.
+data_month = Polynomial('data/crossing_remittance_by_month.xlsx')
 data_year_reg = Polynomial('data/crossings_per_year.xlsx')
 
-# functions plotting data
-# data_month.polynomial_axis()
+# Functions plotting data.
+data_month.polynomial_axis()
 data_year_reg.polynomial_axis()
--- matplotlib_polynomial.py
+++ matplotlib_polynomial.py
@@ -17,46 +17,49 @@ class Polynomial:
         x = self.dataset.iloc[:, :-1].values
         y = self.dataset.iloc[:, -1].values
 
-        # Insert x and y
+        # Function callback.
         train_test_visualize(x, y)
 
 
 def train_test_visualize(x, y):
-    # random numbers in between -1 and 1
-    y_random = np.random.uniform(low=-1, high=1, size=len(y))
+    # Random numbers in between -1 and 1.
+    random_number = np.random.uniform(low=-1, high=1, size=len(y))
 
-    # Split dataset into Train and Test set
+    # Split dataset into train and test set.
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=0)
 
-    # Train the Polynomial model on Training set
+    # Train the polynomial model on training set.
     poly_reg = PolynomialFeatures(degree=4)
     X_poly = poly_reg.fit_transform(X_train)
     regressor = LinearRegression()
     regressor.fit(X_poly, y_train)
 
-    # Predicting Test result
+    # Predicting test results.
     y_pred = regressor.predict(poly_reg.transform(X_test))
     np.set_printoptions(precision=2)
 
-    # Evaluate the R2-value of model
+    # Evaluate the R2-value of model.
     print(r2_score(y_test, y_pred))
 
+    # Extra random generator based on a six-sided dice.
     # Refactor, 1 betyder - 1.0, 2 betyder -0.6, 3 betyder -0.2, 4 betyder +0.2, 5 betyder +0.6, 6 betyder +1
-    # snurrar ett roulette hjul 1000 gånger
-    # för statisktik på vilka nummer ni får... blir de jämt fördelade? Om man kastar tärningen 100000 gånger?
-    # om man snurrar rouletten 1 miljon gånger
+    # Implement roulette wheel one million times statistic on spread of numbers, how?
 
-    xyz = regressor.predict(poly_reg.fit_transform(x))
-    a = xyz + (y_random * 5000000)
-    standard_deviation = xyz.std()
-    print(y_random)
+    # Creates a new array with random numbers.
+    y_reg = regressor.predict(poly_reg.fit_transform(x))
+    y_deviation = y_reg + (random_number * 5000000)
+    standard_deviation = y_reg.std()
+
+    # Print arrays and random number.
     print(y)
-    print(xyz)
+    print(y_reg)
+    print(random_number)
     print(standard_deviation)
 
-    # Visualising the Polynomial Regression results
+    # Visualizing results of the polynomial regression.
     plt.plot(x, y, color='red')
-    plt.plot(x, a, color='blue')
+    plt.plot(x, y_deviation, color='blue')
+    plt.plot(x, y_reg, color='green')
     plt.suptitle('R2: ' + str(r2_score(y_test, y_pred)))
     plt.title('Border Remittance Polynomial')
     plt.xlabel('Year')
